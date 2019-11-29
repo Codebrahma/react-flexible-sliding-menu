a268da9a79425ed848626d4f769f842fa1687eb4
DivyanshBatham <divyansh.sunita@gmail.com>
2019-11-29 17:05:02 +0530

    :sparkles: Library Setup
    - Install & Setup Webpack with Babel

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  externals: {
+    // Use external version of React
+    react: 'react',
+  }
+};
