9f0fdb1734477aedbcc45044bf722464907058b1
DivyanshBatham <divyansh.sunita@gmail.com>
2019-12-02 16:59:16 +0530

    Added library name to webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
+    library: 'react-flexible-sliding-menu',
     libraryTarget: 'umd'
   },
   resolve: { extensions: ['.js', '.jsx'] },
