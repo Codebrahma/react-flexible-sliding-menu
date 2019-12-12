58525e7db2804d2f0e87018c22b3c11d6d0dbe16
DivyanshBatham <divyansh.sunita@gmail.com>
2019-12-13 00:15:16 +0530

    Changed webpack entry file

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
