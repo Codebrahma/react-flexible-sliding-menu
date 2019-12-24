91dd13317a7b39b527f5de443d8e33fd06291b14
DivyanshBatham <divyansh.sunita@gmail.com>
2019-12-24 16:01:52 +0530

    Added CONTRIBUTING.md to docz

--- doczrc.js
+++ doczrc.js
@@ -1,11 +1,12 @@
 export default {
-  files: 'docs/**/*.{md,mdx}',
+  files: ['docs/**/*.{md,mdx}', 'CONTRIBUTING.md'],
   dest: './dist-docs',
   menu: [
     'Introduction',
     'Getting Started',
     { name: 'Animations', menu: ['Slide', 'Push'] },
     { name: 'API', menu: ['MenuProvider', 'MenuContext'] },
-    { name: 'Examples', menu: ['Usage with React Router'] }
+    { name: 'Examples', menu: ['Usage with React Router'] },
+    'Contributing'
   ]
 };
