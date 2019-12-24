72fdbcdd9d17380458bb9a296013771ef1f66502
DivyanshBatham <divyansh.sunita@gmail.com>
2019-12-24 19:35:32 +0530

    Added Logo to Introduction

---
+++ docs/components/LogoContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../svgs/Logo';
+
+const LogoContainer = () => {
+  return (
+    <div style={{ width: '180px', paddingBottom: '1rem' }}>
+      <Logo />
+    </div>
+  );
+};
+
+export default LogoContainer;
